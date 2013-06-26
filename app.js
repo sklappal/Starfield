3d79ea25ccce039a287ff68c435d790295d58b53
Sampsa Lappalainen <sampsa.lappalainen@iki.fi>
2013-06-26 23:09:29 +0300

    Add static points and deterministic physics
    - Add static background. On each frame, there is a possibility that a
    static point is promoted to a movable point, which then drops towards
    the camera. If that happens, a new static point is created.
    - Made the physics loop tick at constant 60 FPS, regardless of the
    animation speed. This solves a problem where when the browser was
    inactive, all stars would've been gone and an empty sky was shown for a
    while.

--- app.js
+++ app.js
@@ -2,23 +2,29 @@ function App() {
   
   var that = this;
   
-  var pointCount = 800;
+  var pointCount = 400;
+  var staticPointCount = 200;
   var velocity = 1;
   
   var WIDTH = GetCanvas().width;
   var HEIGHT = GetCanvas().height;
+  var DEFAULT_Z = 200;
   
-  var curTime = new Date().getTime();
-  var prevTime = 0;
+  var curDrawTime = new Date().getTime();
+  var prevDrawTime = 0;
   
   var colorLUT = [];
   
   function GetFrameTime() {
-    return curTime - prevTime;
+    return curDrawTime - prevDrawTime;
   }
   
   function CreatePoint() {
-    return {pos: [100 * (Math.random() - 0.5), 100 * (Math.random() - 0.5), 200], size: Math.random() * 200};
+    return {pos: [100 * (Math.random() - 0.5), 100 * (Math.random() - 0.5), DEFAULT_Z], size: (Math.random() * 200) + 50};
+  }
+  
+  function CreateStaticPoint() {
+    return {onScreen: [WIDTH * Math.random(), HEIGHT * Math.random()], size: 2 * Math.random(), color: colorLUT[Math.floor(172*Math.random())]};
   }
   
   function Init() {
@@ -39,13 +45,30 @@ function App() {
       that.points[i].pos[2] *= Math.random(); // Start with random Z
     }
     
-    that.points.sort(function(p1, p2) { return p1.pos[2] - p2.pos[2]; })
+    that.points.sort(function(p1, p2) { return p1.pos[2] - p2.pos[2]; });
     
+    that.staticPoints = [];
+    
+    for (var i = 0; i < staticPointCount; i++) {
+      that.staticPoints.push(CreateStaticPoint());
+    }
   }
   
   function StillVisible(point) {
     var onScreen = WorldToScreen(point);
     return point[2] > 0 && onScreen[0] >= 0 && onScreen[1] >= 0 && onScreen[0] < WIDTH && onScreen[1] < HEIGHT;
+  }
+  
+  function len(point) {
+    return Math.sqrt(point[0]*point[0] + point[1]*point[1] + point[2]*point[2]);
+  }
+  
+  function MapToMovingPoint(staticPoint) {
+    var pos = [];
+    pos[0] = ((staticPoint.onScreen[0] / WIDTH) - 0.5) * DEFAULT_Z;
+    pos[1] = ((staticPoint.onScreen[1] / HEIGHT) - 0.5) * DEFAULT_Z;
+    pos[2] = DEFAULT_Z;
+    return { pos: pos, size: staticPoint.size * len(pos), color: staticPoint.color};
   }
   
   function WorldToScreen(ob) {
@@ -66,17 +89,28 @@ function App() {
     var ctx = GetContext();
     ctx.clearRect(0, 0, WIDTH, HEIGHT);    
 
-    for (var i = that.points.length-1; i >= 0; i--) { // Draw from back to front
-      var onScreen = WorldToScreen(that.points[i].pos);
-      var pos = that.points[i].pos;
-      var scaler = 1.0 / Math.sqrt(pos[0] * pos[0] + pos[1] * pos[1] + pos[2] * pos[2]);
-      var rgb = Math.min(255, Math.floor(255.0*scaler*25));
-      
-      var color = colorLUT[rgb];
-      
-      FilledCircle(onScreen[0], onScreen[1], that.points[i].size * scaler, color);
+    for (var i = 0; i < that.staticPoints.length; i++) {
+      var pnt = that.staticPoints[i];
+      FilledCircle(pnt.onScreen[0], pnt.onScreen[1], pnt.size, pnt.color);
     }
-
+    
+    for (var i = that.points.length-1; i >= 0; i--) { // Draw from back to front
+      var pnt = that.points[i];
+      var pos = pnt.pos;
+      var onScreen = WorldToScreen(pos);
+      var scaler = 1.0 / len(pos);
+      var rgb = Math.min(255, Math.floor(255.0*scaler*25));
+      var size = pnt.size * scaler;
+      if (rgb > 1 && size >= 0.5) {
+        
+        var color = pnt.color;
+        if (color == undefined) {
+          color = colorLUT[rgb];
+        } 
+        
+        FilledCircle(onScreen[0], onScreen[1], size, color);
+      }
+    }
   }
   
   function DrawOverlay() {
@@ -102,16 +136,33 @@ function App() {
     }
   }();
   
+  var prevPhysTime = 0;
+  var curPhysTime = new Date().getTime();
+  
+  function TickPhysics() {
+    prevPhysTime = curPhysTime;
+    curPhysTime = new Date().getTime();
+    Animate(curPhysTime-prevPhysTime);
+    setTimeout(TickPhysics, 1000/60);
+  }
    
-  function Animate() {
+  function Animate(elapsed) {
+    
     var newPoints = [];
-    var elapsed = GetFrameTime();
+    
+    var movement = velocity*elapsed*0.1;
     for (var i = 0; i < that.points.length; i++) {
-      that.points[i].pos[2] -= velocity * elapsed * 0.1;
+      that.points[i].pos[2] -= movement;
       
       if (StillVisible(that.points[i].pos)) {
         newPoints.push(that.points[i]);
       }
+    }
+    
+    if (Math.random() < 0.1) {
+      newPoints.push(MapToMovingPoint(that.staticPoints[0]));
+      that.staticPoints.shift(); // Remove first
+      that.staticPoints.push(CreateStaticPoint());
     }
     
     while (newPoints.length < pointCount) {
@@ -122,10 +173,10 @@ function App() {
   }
   
   function FilledCircle(posx, posy, radius, color) {
-    var ctx = GetContext();
+    var ctx = that.context;
     ctx.beginPath();
     var counterClockwise = false;
-    ctx.arc(posx, posy, radius, 0, 2 * Math.PI, counterClockwise);
+    ctx.arc(posx, posy, radius, 0, 2 * Math.PI, false);
     ctx.fillStyle = color;
     ctx.fill();
   }
@@ -143,11 +194,10 @@ function App() {
   
   function tick() {
     requestAnimFrame(tick);
-    prevTime = curTime;
-    curTime = new Date().getTime();
+    prevDrawTime = curDrawTime;
+    curDrawTime = new Date().getTime();
     Draw();
     DrawOverlay();
-    Animate();
   }
   
   this.Start = function() {
@@ -157,7 +207,9 @@ function App() {
     window.onresize = OnResize;
     OnResize();
     Init();
+    that.context = GetContext();
     tick();
+    TickPhysics();
   }
   
   function OnResize() {    
